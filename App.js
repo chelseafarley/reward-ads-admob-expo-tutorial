ed403cb07003402e41ec11eb1087b1251c913528
Chelsea Farley <chelseafarley91@gmail.com>
2022-02-17 19:02:26 +1300

    Rewarded ad admob

--- App.js
+++ App.js
@@ -1,10 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Platform, Button } from 'react-native';
+import { AdMobRewarded } from 'expo-ads-admob';
+import React from 'react';
 
 export default function App() {
+  React.useEffect(() => {
+    return function cleanup() {
+      AdMobRewarded.removeAllListeners();
+    };
+  });
+
+  let adUnitId = Platform.select({
+    ios: "ca-app-pub-3940256099942544/1712485313",
+    android: "ca-app-pub-3940256099942544/5224354917"
+  });
+
+  let loadAd = async () => {
+    await AdMobRewarded.setAdUnitID(adUnitId);
+    await AdMobRewarded.requestAdAsync();
+  };
+
+  AdMobRewarded.addEventListener("rewardedVideoUserDidEarnReward", (reward) => {
+    console.log(reward);
+    loadAd();
+  });
+
+  AdMobRewarded.addEventListener("rewardedVideoDidFailToLoad", () => {
+    loadAd();
+  });
+
+  AdMobRewarded.addEventListener("rewardedVideoDidDismiss", () => {
+    loadAd();
+  })
+
+  loadAd();
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Button title='Show Reward Ad' onPress={() => { AdMobRewarded.showAdAsync() }} />
       <StatusBar style="auto" />
     </View>
   );
